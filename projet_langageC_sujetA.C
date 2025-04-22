ef0aeb2e5f10be7ffa98c05ddbf1f171b8d06af8
Gabbro-EXIA <thiblob2912@gmail.com>
2025-04-22 15:59:43 +0200

    nier

--- projet_langageC_sujetA.C
+++ projet_langageC_sujetA.C
@@ -1,14 +1,109 @@
-int main(){
-    menu();
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void menu(){
-    printf("+-----------------------------------+\n
-            |1 : Addition                       |\n
-            |2 : Soustraction                   |\n 
-            |3 : Multiplication                 |\n 
-            |4 : Tables des multiplications     |\n
-            |5 : Divisions                      |\n
-            |0 : Sortir du jeu                  |\n
-            +-----------------------------------+");
+    printf("+-----------------------------------+\n|1 : Addition                       |\n|2 : Soustraction                   |\n|3 : Multiplication                 |\n|4 : Tables des multiplications     |\n|5 : Divisions                      |\n|0 : Sortir du jeu                  |\n+-----------------------------------+\nQuel est votre choix ?\t");
+}
+
+
+void addition(int *score){
+    int resultat;
+    srand(time(NULL));
+    int a = rand() % 100;
+    int b = rand() % 100;
+
+    printf("Addition \n %d + %d = ? \n Entrez le resultat \n", a, b);
+    scanf("%d", &resultat);
+    printf("Vous avez entre %d \n", resultat);
+    if (resultat == a + b){
+        printf("Bravo ! \n");
+        score += 1;
+    }
+    else{
+        printf("Trompe ! \n");
+    }
+}
+
+
+void soustraction(int *score){
+    int resultat;
+    while(resultat == NULL){
+    srand(time(NULL));
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if(a > b){
+            printf("Soustraction \n %d - %d = ? \n Entrez le resultat \n", a, b);
+            scanf("%d", &resultat);
+            printf("Vous avez entre %d \n", resultat);
+            if (resultat == a - b){
+                printf("Bravo ! \n");
+                score += 1;
+            }
+            else{
+                printf("Trompe ! \n");
+            }
+        }
+    }
+}
+
+
+void multiplication(int *score){
+    int resultat;
+    srand(time(NULL));
+    int a = rand() % 10 + 1;
+    int b = rand() % 10 + 1;
+
+    printf("Multiplication \n %d * %d = ? \n Entrez le resultat \n", a, b);
+    scanf("%d", &resultat);
+    printf("Vous avez entre %d \n", resultat);
+    if (resultat == a * b){
+        printf("Bravo ! \n");
+        score += 1;
+    }
+    else{
+        printf("Trompe ! \n");
+    }
+}
+
+
+void table_multi(int *score){
+    
+}
+
+
+
+int main(){
+    char choix;
+    int score = 0;
+
+    menu();
+    scanf("%s", &choix);
+    while(choix != '0'){
+        switch(choix){
+            case '1':
+                addition(&score);
+                break;
+            case '2':
+                soustraction(&score);
+                break;
+            case '3':
+                multiplication(&score);
+                break;
+            case '4':
+                printf("table de multiplication");
+                break;
+            case '5':
+                printf("division");
+                break;
+            default:
+                printf("Mauvaise manipulation");
+        }
+        printf("%d",&score);
+        menu();
+        scanf("%s", &choix);
+        printf("");
+    }
+    printf("Merci de votre visite ! \n");
+    return 0;
 }
