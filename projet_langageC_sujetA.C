ab7e69f72d2c6643d8fe87fcdbf4d89df2776163
Gabbro-EXIA <thiblob2912@gmail.com>
2025-04-22 14:29:51 +0200

    oui

---
+++ projet_langageC_sujetA.C
@@ -0,0 +1,14 @@
+int main(){
+    menu();
+}
+
+void menu(){
+    printf("+-----------------------------------+\n
+            |1 : Addition                       |\n
+            |2 : Soustraction                   |\n 
+            |3 : Multiplication                 |\n 
+            |4 : Tables des multiplications     |\n
+            |5 : Divisions                      |\n
+            |0 : Sortir du jeu                  |\n
+            +-----------------------------------+");
+}
